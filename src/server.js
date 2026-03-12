53f87272e4efdedd6e1750f77b1b32f30cdb36f2
rafideveloper7 <rafideveloper7@gmail.com>
2026-03-12 18:10:11 +0500

    update server.js

--- src/server.js
+++ src/server.js
@@ -9,10 +9,12 @@ const PORT = process.env.PORT || 5000;
 // Connect to database
 connectDB();
 
-app.listen(PORT, () => {
-  console.log(`
-    Server running on port ${PORT}
-    Environment: ${process.env.NODE_ENV || 'development'}
-    API: http://localhost:${PORT}/api
-  `);
-});
+// app.listen(PORT, () => {
+//   console.log(`
+//     Server running on port ${PORT}
+//     Environment: ${process.env.NODE_ENV || 'development'}
+//     API: http://localhost:${PORT}/api
+//   `);
+// });
+
+export default app;
